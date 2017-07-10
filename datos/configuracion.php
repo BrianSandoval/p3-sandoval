a1e6b3c00b05799a155a3e726ab4801e24110e33
brianSandoval <brian_del2009@hotmail.com>
2017-07-09 23:15:03 -0500

    .

--- datos/configuracion.php
+++ datos/configuracion.php
@@ -8,8 +8,8 @@
   define("NOMBRE_BD", "d2i47eueo96v96");
  */
 
-define("SERVIDOR_BD", "localhost");
-define("USUARIO_BD", "postgres");
-define("CLAVE_BD", "0907");
+define("SERVIDOR_BD", "ec2-50-19-83-146.compute-1.amazonaws.com");
+define("USUARIO_BD", "oaxnhkpblpwfpp");
+define("CLAVE_BD", "d45bea2c78852bb450495f6bc46f3a0043d12431f0c7c5689daa24786a65443d");
 define("PUERTO_BD", "5432");
-define("NOMBRE_BD", "finalPrograIII");
+define("NOMBRE_BD", "d8djo15k80ne42");
